774af47bb6702de2317892bb622ea2d6655fa619
mbaumaister13 <mbaumaister13@gmail.com>
2026-08-11 19:35:59 -0400

    IFrame POC

--- apps/host/App.tsx
+++ apps/host/App.tsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const TRUSTED_ORIGIN = 'http://localhost:5555';
+
+  useEffect(() => {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    const handleMessageFromIframe = (event: any) => {
+      if (event.origin !== TRUSTED_ORIGIN) {
+        return;
+      }
+
+      console.log('Host app received event', event.data);
+    };
+
+    window.addEventListener('message', handleMessageFromIframe);
+    return () => window.removeEventListener('message', handleMessageFromIframe);
+  }, []);
+
+  const sendMessageToIframe = () => {
+    if (iframeRef.current && iframeRef.current.contentWindow) {
+      const payload = {
+        type: 'HOST',
+        text: 'HELLO WORLD'
+      }
+
+      iframeRef.current.contentWindow.postMessage(payload, TRUSTED_ORIGIN);
+    }
+  }
 
   return (
     <>
@@ -21,102 +47,18 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={ () => setCount((count) => count + 1) }
-        >
-          Count is { count }
+        <button type="button"
+                className="counter"
+                onClick={ sendMessageToIframe }>
+          Say hi to iframe
         </button>
       </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={ viteLogo } alt=""/>
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={ reactLogo } alt=""/>
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-
-      <iframe></iframe>
+      <iframe ref={ iframeRef }
+              src={ TRUSTED_ORIGIN }
+              title="Data App"
+              sandbox="allow-scripts allow-same-origin">
+      </iframe>
     </>
   )
 }
--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,20 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+        },
     },
-  },
 ])
